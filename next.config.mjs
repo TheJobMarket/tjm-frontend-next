7f67f44023814d4b5b8b160f9d9269e6e2bab1df
viktaur <victorgabal@gmail.com>
2024-08-04 09:21:35 +0100

    basic redirect

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async redirects() {
+        return [
+            {
+                source: '/',
+                destination: '/en/jobs',
+                permanent: true,
+            },
+        ];
+    },
+};
 
 export default nextConfig;
