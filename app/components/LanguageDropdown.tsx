eacea44feeb1e4f9b13901a691f8b44a84d95995
viktaur <victorgabal@gmail.com>
2024-08-05 11:47:39 +0100

    language dropdown menu

--- app/components/LanguageDropdown.tsx
+++ app/components/LanguageDropdown.tsx
@@ -1,10 +1,28 @@
+"use client"
+
 import styles from "./LanguageDropdown.module.css";
 import React from "react";
+import "../globals.css"
 
 export default function LanguageDropdown() {
+
+    function showDropdown() {
+        document.getElementById("dropdownContent")!.style.display = "block";
+    }
+
+    function hideDropdown() {
+        document.getElementById("dropdownContent")!.style.display = "none";
+    }
+
     return (
-        <div className={styles.langId}>
-            EN
+        <div id="dropdown" onMouseEnter={showDropdown} onMouseLeave={hideDropdown} className={styles.dropdown}>
+            <button className={styles.dropbtn}>EN</button>
+            <div id="dropdownContent" className={styles.dropdownContent}>
+                <div className={styles.linkContainer}>
+                    <a href="#">FR</a>
+                    <a href="#">ES</a>
+                </div>
+            </div>
         </div>
     )
 }
